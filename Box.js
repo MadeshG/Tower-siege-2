6270db82cae4f5048a6bc266fd5012f113966ae8
MadeshG <74277922+MadeshG@users.noreply.github.com>
2021-01-24 19:45:58 -0600

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,35 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.3,
+        'friction':0.1,
+        'density':0.1
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    console.log('Box X= '+x+'  Y='+y);
+    World.add(world, this.body);
+    this.visibilty=255
+  }
+  display(){
+    if(this.body.speed<2){
+
+    
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill("red");
+    rect(0, 0, this.width, this.height);
+    pop();
+    }
+else{
+  World.remove(world,this.body)
+  this.visibilty=this.visibilty-5
+}
+
+  }
+};
